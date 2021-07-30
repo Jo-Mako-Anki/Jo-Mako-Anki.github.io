ad1ab53e3505aa8ae8b684d0a7802da5a5b63125
Jo-Mako-Anki <86222694+Jo-Mako-Anki@users.noreply.github.com>
2021-07-30 17:05:13 +0200

    Add files via upload

--- js/highlight_active_page.js
+++ js/highlight_active_page.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var $page = location.pathname;
-  const matches = document.querySelectorAll(
-    "a.navbar__menu--links, a.sidebar-container__links"
-  );
-  matches.forEach(function (link) {
-    var $link = "/" + link.getAttribute("href");
-    if ($page.endsWith($link)) {
+  var page = location.pathname;
+  const matches = document.querySelectorAll("a.navbar__menu--links, a.sidebar-container__links");
+  matches.forEach((link) => {
+    var link_end = link.getAttribute("href");
+    var link_end = link_end.split("/");
+    var link_end = link_end[link_end.length - 1];
+    var link_end = "/" + link_end;
+
+    if (page.endsWith(link_end)) {
       link.classList.add("active-page");
     } else {
       link.classList.remove("active-page");
+    }
+    if (!page.endsWith(".html")) {
+      matches[0].classList.add("active-page");
     }
   });
 });
